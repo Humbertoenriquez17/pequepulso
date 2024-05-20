5643f1097c5ad93126a4daa7c3a5b74336be035e
Humberto A. Enriquez <59264551+Humbertoenriquez17@users.noreply.github.com>
2024-05-20 07:47:06 -0600

    Updates

---
+++ _next/static/chunks/app/page-8982c6669d3fe3fc.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{359:function(n,e,t){Promise.resolve().then(t.t.bind(t,5250,23)),Promise.resolve().then(t.t.bind(t,3145,23))},3145:function(){}},function(n){n.O(0,[250,971,69,744],function(){return n(n.s=359)}),_N_E=n.O()}]);
